d32c10c3e116c540ccca936115cf6e1e5bcb5300
nickelswitte <witte.nickels-tim17@it.dhbw-ravensburg.de>
2021-08-22 23:20:52 +0200

    Added footer Added Camera page

--- app.js
+++ app.js
@@ -31,63 +31,25 @@ app.use(express.static('public'));
 
 
 // Serve the index page
-app.get('/', (req, res) => {
-    res.render("index", {
-        title: "This is the title",
-        subtitle: "This is the subtitle"
-    })
-})
-
-// Serve the index page
 app.get('/me', (req, res) => {
     res.render("_about", {
         profilePicturePath: "/img/IMG_00034.jpg"
     })
 })
 
-let data = {
-    title: "Find here some images",
-    subtitle: "They are just for you",
-    people: [
-        {
-            name: "John Taylor",
-            twitter: "jtee03",
-            description: "Wafer caramels danish bonbon. Cheesecake cookie sugar plum. I love ice cream pastry.",
-            imagelink: "https://i.imgur.com/2F3Al82.jpeg"
-        },
-        {
-            name: "Doreen Johnson",
-            twitter: "dorydance-xt",
-            description: "Cotton candy biscuit tootsie roll chupa chups I love jujubes lollipop. Dessert cheesecake I love I love tart chocolate cake brownie",
-            imagelink: "https://i.imgur.com/aUhpEz3.jpeg"
-        },
-        {
-            name: "Freerk Jennsen",
-            twitter: "freerk",
-            description: "Lorem Ipsum Bla Blop",
-            imagelink: "https://i.imgur.com/QdOTqIR.jpeg"
-        },
-        {
-            name: "Milli Gerogia",
-            twitter: "septemberagain",
-            description: "Cupcake ipsum dolor sit amet tiramisu. Tiramisu tiramisu muffin. Liquorice chocolate cake bear claw sweet apple pie I love halvah tart. Toffee marshmallow croissant powder. Wafer caramels danish bonbon. Cheesecake cookie sugar plum. I love ice cream pastry. Cotton candy biscuit tootsie roll chupa chups I love jujubes lollipop. Dessert cheesecake I love I love tart chocolate cake brownie.",
-            imagelink: "https://i.imgur.com/sBM9y7a.png"
-        },
-        {
-            name: "Lülli Widde",
-            twitter: "vanilly",
-            description: "Cupcake ipsum dolor sit amet tiramisu. Tiramisu tiramisu muffin. Liquorice chocolate cake bear claw sweet apple pie I love halvah tart. Toffee marshmallow croissant powder. Wafer caramels danish bonbon. Cheesecake cookie sugar plum.",
-            imagelink: "https://i.imgur.com/iEMP5I6.jpeg"
-        },
-    ]
-}
-
-
-app.get('/i', (req, res) => {
-    res.render("images", data)
+// Serve the index page
+app.get('/kamera', (req, res) => {
+    res.render("_camera", {
+        picture1: "/img/camera.jpg",
+        picture2: "/img/camera2.jpg",
+        picture3: "/img/objektiv.jpg"
+    })
 })
 
-app.get('/ig', (req, res) => {
+
+
+
+app.get('/', (req, res) => {
 
     const testFolder = './public/img/bornholm/';
     
@@ -107,79 +69,6 @@ app.get('/ig', (req, res) => {
     });
 
     
-});
-
-
-// Serve the index page
-app.get('/m/:filepath(([a-zA-Z0-9]+(\/)?)*)', (req, res, next) => {
-
-    var path = "";
-
-    // check for the path
-    if (typeof req.params[0] !== 'undefined') {
-        path += req.params[0];
-    } 
-
-    // Build it
-    path += req.params[2];
-
-    //console.log(path);
-    //console.log(req.params);
-
-    let pathToFile = __dirname + '\\public\\md\\' + path + '.md';
-
-    // console.log(pathToFile);
-    
-
-    fs.readFile(pathToFile, 'utf8' , (err, data) => {
-        if (err) {
-            console.error("FS Error" + err);
-            return;
-        }
-
-        if (typeof data !== 'string') {
-            return;
-        }
-
-        let pathForBreadcrumb = path.split('\/');
-        
-
-        let markdownParsed = md.render(data);
-
-        // console.log(data)
-        res.render("markdown", {
-            markdown: markdownParsed,
-            title: "Markdown Render",
-            subtitle: "This is auto-generated from Markdown",
-            breadcrumb: pathForBreadcrumb
-        });
-    });
-    
-});
-
-app.get('/t', (req, res) => {
-    res.render("tiles", {
-        title: 'Tiles Example',
-        subtitle: 'How tiles can be used for nice things'
-    })
-})
-
-app.get('/p', (req, res) => {
-    res.render("Password", {
-        title: 'Secret Password Example',
-        subtitle: 'Hidden entrance'
-    })
-})
-
-app.post('/pw', (req, res) => {
-    var string = "";
-    var keys = req.body.value;
-
-    keys.forEach(function(key) {
-        string += String.fromCharCode(key);
-    });
-
-    console.log(string);
 });
 
 
