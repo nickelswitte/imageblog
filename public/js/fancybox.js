7ef17dfc0d3d70fb2737779c41a9cb7ac88b438a
nickelswitte <witte.nickels-tim17@it.dhbw-ravensburg.de>
2021-11-10 13:11:12 +0100

    Removed Scrollbar from FancyBox

--- public/js/fancybox.js
+++ public/js/fancybox.js
@@ -2,16 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
   
   Fancybox.defaults.Hash = false;
 
-  // #1 Grouped Images
   Fancybox.bind(".card-image a", {
     infinite: false,
-    hideScrollbar: true,
     groupAll: true,
+    hideScrollbar: true,
     Toolbar: {
       display: [
-        { id: "prev", position: "center" },
-        { id: "counter", position: "center" },
-        { id: "next", position: "center" },
         "slideshow",
         "fullscreen",
         "thumbs",
